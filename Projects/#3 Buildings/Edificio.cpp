51d032e1c968f44f2858a470b19d2599765f193d
Fátima Rojo <11411647+serfati5@users.noreply.github.com>
2019-10-04 11:14:24 -0500

    Create Edificio.cpp

---
+++ Projects/#3 Buildings/Edificio.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+#include <iomanip>
+#include "Edificio.h"
+
+//tEdificio nuevoEdificio() {}
+//void mostrarEdificio(tEdificio edificio) {}
